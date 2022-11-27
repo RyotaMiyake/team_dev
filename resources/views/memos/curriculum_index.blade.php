97f57ed4ebeac910491f4df8ba63e331477e80ae
cgud3061 <cgud3061@mail4.doshisha.ac.jp>
2022-11-27 17:30:43 +0900

    トップページのレイアウト編集

--- resources/views/memos/curriculum_index.blade.php
+++ resources/views/memos/curriculum_index.blade.php
@@ -8,20 +8,32 @@
             <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         </head>
         <body>
-            <h1>{{ $curriculum->curriculum }}</h1>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1>{{ $curriculum->curriculum }}</h1>
+                </div>
+            </div>
             <div>
                 @foreach ($memos as $memo)
                     @if($memo->curriculum->id == $curriculum->id)
-                        <div>
-                            <h1>
-                                <a href="/memos/{{ $memo->id }}">{{ $memo->title }}</a>
-                            </h1>
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 bg-white border-b border-gray-200">
+                                <div>
+                                    <h1>
+                                        <a class="text-3xl" href="/memos/{{ $memo->id }}">{{ $memo->title }}</a>
+                                        <p>投稿者：{{ $memo->user->name }}</p>
+                                        <p>投稿日時：{{ $memo->created_at }}</p>
+                                    </h1>
+                                </div>
+                            </div>
                         </div>
                     @endif
                 @endforeach
             </div>
-            <div class="footer">
-                <a href="/dashboard">戻る</a>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="/dashboard">戻る</a>
+                </div>
             </div>
         </body>
     </html>
