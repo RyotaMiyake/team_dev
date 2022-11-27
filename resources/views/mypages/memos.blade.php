5188e50bb51f1116bff274e27daa95a80500b838
RyotaMiyake <xxxryoutaxxx0702@gmail.com>
2022-11-27 12:58:25 +0900

    ナビゲーションの追加

--- resources/views/mypages/memos.blade.php
+++ resources/views/mypages/memos.blade.php
@@ -25,7 +25,7 @@
             <!-- 備忘録一覧 -->
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <p class='memos'>備忘録一覧</p> 
+                    <p class='memos'>備忘録一覧</p>
                     <div class='memos'>
                         @foreach ($memos as $memo)
                             @if ($memo->user_id == $memo->id)
--- routes/web.php
+++ routes/web.php
@@ -25,29 +25,28 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::controller(PostController::class)->middleware(['auth'])->group(function(){
-    Route::get('/', 'index')->name('index');
-    Route::post('/posts', 'store')->name('store');
-    Route::get('/posts/create', 'create')->name('create');
-    Route::get('/posts/{post}', 'show')->name('show');
-    Route::put('/posts/{post}', 'update')->name('update');
-    Route::delete('/posts/{post}', 'delete')->name('delete');
-    Route::get('/posts/{post}/edit', 'edit')->name('edit');
+    Route::get('/', 'index')->name('post.index');
+    Route::post('/posts', 'store')->name('post.store');
+    Route::get('/posts/create', 'create')->name('post.create');
+    Route::get('/posts/{post}', 'show')->name('post.show');
+    Route::put('/posts/{post}', 'update')->name('post.update');
+    Route::delete('/posts/{post}', 'delete')->name('post.delete');
+    Route::get('/posts/{post}/edit', 'edit')->name('post.edit');
 });
 
 Route::controller(CategoryController::class)->middleware(['auth'])->group(function(){
-    Route::get('/categories/{category}','index')->name('index');
+    Route::get('/categories/{category}','index')->name('category.index');
 });
 
 Route::controller(QuestionController::class)->middleware(['auth'])->group(function(){
-    Route::get('/questions', 'index')->name('index');
-    Route::post('/questions', 'store')->name('store');
-    Route::get('/questions/create', 'create')->name('create');
-    Route::get('/questions/{question}', 'show')->name('show');
-    Route::put('/questions/{question}', 'update')->name('update');
-    Route::delete('/questions/{question}', 'delete')->name('delete');
-    Route::get('/questions/{question}/edit', 'edit')->name('edit');
+    Route::get('/questions', 'index')->name('question.index');
+    Route::post('/questions', 'store')->name('question.store');
+    Route::get('/questions/create', 'create')->name('question.create');
+    Route::get('/questions/{question}', 'show')->name('question.show');
+    Route::put('/questions/{question}', 'update')->name('question.update');
+    Route::delete('/questions/{question}', 'delete')->name('question.delete');
+    Route::get('/questions/{question}/edit', 'edit')->name('question.edit');
 });
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
